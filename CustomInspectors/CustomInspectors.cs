205f5b5e2c10d7ab0ec759308aaecfc9a532772d
art0007i <art0007i@gmail.com>
2022-06-24 18:16:35 +0200

    Add project files.

---
+++ CustomInspectors/CustomInspectors.cs
@@ -0,0 +1,134 @@
+using HarmonyLib;
+using NeosModLoader;
+using System;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using FrooxEngine;
+using SpecialItemsLib;
+using System.IO;
+using BaseX;
+
+namespace CustomInspectors
+{
+    public class CustomInspectors : NeosMod
+    {
+        public override string Name => "CustomInspectors";
+        public override string Author => "art0007i";
+        public override string Version => "1.0.0";
+        public override string Link => "https://github.com/art0007i/CustomInspectors/";
+        public override void OnEngineInit()
+        {
+            Harmony harmony = new Harmony("me.art0007i.CustomInspectors");
+            OurItem = SpecialItemsLib.SpecialItemsLib.RegisterItem(INSPECTOR_TAG);
+            harmony.PatchAll();
+
+        }
+        private static string INSPECTOR_TAG { get { return "custom_inspector_panel"; } }
+        private static CustomSpecialItem OurItem;
+
+        [HarmonyPatch(typeof(SlotHelper), "GenerateTags", new Type[] { typeof(Slot), typeof(HashSet<string>) })]
+        class SlotHelper_GenerateTags_Patch
+        {
+            static void Postfix(Slot slot, HashSet<string> tags)
+            {
+                var correct = slot.Name == "Holder" ? slot[0] : slot;
+                if (correct.GetComponent<SceneInspector>() != null)
+                {
+                    tags.Add(INSPECTOR_TAG);
+                }
+            }
+        }
+        [HarmonyPatch(typeof(SceneInspector), "OnAttach")]
+        class SceneInspector_OnAttach_Patch
+        {
+            static bool Prefix(SceneInspector __instance)
+            {
+                if (OurItem.Uri == null) return true;
+                var translator = new ReferenceTranslator();
+                var uri = OurItem.Uri;
+                if ( uri.Scheme == "neosrec")
+                {
+                    var ctask = Engine.Current.Cloud.GetRecordCached<Record>(uri, null);
+                    ctask.Wait();
+                    CloudX.Shared.CloudResult<Record> cloudResult = ctask.Result;
+                    if (cloudResult.IsError)
+                    {
+                        return true;
+                    }
+                    uri = new Uri(cloudResult.Entity.AssetURI);
+                }
+                var ttask = Engine.Current.AssetManager.RequestGather(uri, FrooxEngine.Priority.Normal, null);
+                ttask.AsTask().Wait();
+                string text = ttask.Result;
+                if (text != null && File.Exists(text))
+                {
+                    // this is where we load the json and parse it to merge guids with refids
+                    DataTreeDictionary node = DataTreeConverter.Load(text, uri);
+                    var rootNode = node.TryGetDictionary("Object");
+                    if (rootNode.TryGetDictionary("Name").TryGetNode("Data").LoadString() == "Holder")
+                    {
+                        rootNode = rootNode.TryGetList("Children").Children[0] as DataTreeDictionary;
+                        node.Children["Object"] = rootNode;
+                    }
+                    var topLevel = rootNode.TryGetDictionary("Components").TryGetList("Data");
+                    foreach (var dataNode in topLevel.Children)
+                    {
+                        var dictNode = (dataNode as DataTreeDictionary);
+                        var str = dictNode.TryGetNode("Type").LoadString();
+                        if (str == typeof(SceneInspector).ToString())
+                        {
+                            var dataDict = dictNode.TryGetDictionary("Data");
+
+                            // Component guid merge
+                            translator.Associate(__instance.ReferenceID, new Guid(dataDict.TryGetNode("ID").LoadString()));
+                            dataDict.Children["ID"] = new DataTreeValue(Guid.NewGuid().ToString());
+
+                            __instance.ForeachSyncMember<IWorldElement>((member) =>
+                            {
+                                var guidStr = dataDict.TryGetDictionary(member.Name)?.TryGetNode("ID").LoadString();
+                                if (guidStr != null)
+                                {   
+                                    // Sync member guids merge
+                                    translator.Associate(member.ReferenceID, new Guid(guidStr));
+                                    dataDict.TryGetDictionary(member.Name).Children["ID"] = new DataTreeValue(Guid.NewGuid().ToString());
+                                }
+                            });
+                            break;
+                        }
+                    }
+                    // now time to actually load the object
+                    if (!__instance.Slot.IsDestroyed)
+                    {
+                        var pos = __instance.Slot.GlobalPosition;
+                        var rot = __instance.Slot.GlobalRotation;
+                        var scl = __instance.Slot.GlobalScale;
+
+                        __instance.Slot.LoadObject(node, refTranslator: translator);
+                        var old = __instance.Slot.GetComponent<SceneInspector>((insp) => insp != __instance);
+
+                        var rt = AccessTools.Field(typeof(SceneInspector), "_rootText");
+                        var ct = AccessTools.Field(typeof(SceneInspector), "_componentText");
+                        var hcr = AccessTools.Field(typeof(SceneInspector), "_hierarchyContentRoot");
+                        var ccr = AccessTools.Field(typeof(SceneInspector), "_componentsContentRoot");
+
+                        (rt.GetValue(__instance) as SyncRef<Sync<string>>).Target = (rt.GetValue(old) as SyncRef<Sync<string>>).Target;
+                        (ct.GetValue(__instance) as SyncRef<Sync<string>>).Target = (ct.GetValue(old) as SyncRef<Sync<string>>).Target;
+                        (hcr.GetValue(__instance) as SyncRef<Slot>).Target = (hcr.GetValue(old) as SyncRef<Slot>).Target;
+                        (ccr.GetValue(__instance) as SyncRef<Slot>).Target = (ccr.GetValue(old) as SyncRef<Slot>).Target;
+
+                        old.Destroy(false);
+
+                        __instance.Enabled = true;
+                        __instance.Slot.GlobalPosition = pos;
+                        __instance.Slot.GlobalRotation = rot;
+                        __instance.Slot.GlobalScale = scl;
+
+                    }
+                }
+                return false;
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# CustomInspectors
+
+A [NeosModLoader](https://github.com/zkxs/NeosModLoader) mod for [Neos VR](https://neos.com/) that allows you to favorite inspector panels by saving them and clicking the purple heart.<br>
+Make sure to wait for it to finish syncing before favoriting or it won't work!
+
+## Installation
+1. Install [NeosModLoader](https://github.com/zkxs/NeosModLoader).
+1. Place [CustomInspectors.dll](https://github.com/art0007i/CustomInspectors/releases/latest/download/CustomInspectors.dll) into your `nml_mods` folder. This folder should be at `C:\Program Files (x86)\Steam\steamapps\common\NeosVR\nml_mods` for a default install. You can create it if it's missing, or if you launch the game once with NeosModLoader installed it will create the folder for you.
+1. Start the game. If you want to verify that the mod is working you can check your Neos logs.
